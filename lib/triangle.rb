f2b6f4b53f5a7a117486a2f94d5da1a2795fd1fa
Kish Siva <rodes88@msn.com>
2019-05-10 18:44:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ class Triangle
     
      
   def kind
+<<<<<<< HEAD
     if (@holder.min<=0) 
         raise TriangleError
     elsif @holder.min + @holder.sort[1] <= @holder.max
@@ -24,15 +25,34 @@ class Triangle
     elsif @holder.uniq.size == 2
       kind = :isosceles
     elsif @holder.uniq.size == 3
+=======
+    if (@holder.max - @holder.min <= @holder.sort[1]) || (@holder.min < 0)
+      begin
+        raise TriangleError
+        rescue TriangleError => error
+          puts error.message
+      end  
+    end 
+    
+    if @holder.uniq.size <= 1
+      kind = :equilateral
+    elsif @holder.uniq.size = 2
+      kind = :isosceles
+    elsif @holder.uniq.size = 3
+>>>>>>> d90448ebdfda80bc4f35a10bb3499c90048237c5
       kind = :scalene
     end 
     
   end 
   
   class TriangleError < StandardError
+<<<<<<< HEAD
     def message 
       "puts error here"
     end
+=======
+    "error message here"
+>>>>>>> d90448ebdfda80bc4f35a10bb3499c90048237c5
   end 
   
 end
